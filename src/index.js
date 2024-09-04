0fe1218a9beed38723449d645d276a75e03e2a5a
Stream5937 <149094480+Stream5937@users.noreply.github.com>
2024-09-04 08:20:54 +0100

    display blocks set

--- src/index.js
+++ src/index.js
@@ -44,11 +44,19 @@ function getData (query) {
     let noData = false;
     let dataResult = null;
     console.log(`lastQuery: ${lastQuery}`);
+    const dateStart = new Date();         //to get start time
+    const t_start = dateStart.getTime();  //record start of fetch action
+    let dateFinish = null;      //to get finish time
+    let t_finish = null;        //record finish of fetch action
+    let t_taken = null ;        //to calculate timing of fetch operation in milliseconds
     fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=SHSBBCG24LZCJNG5DWNL5ZFP2`, 
       {mode: 'cors'}
     )
-      .then(function(response) {return response.json();
+      
+      .then(function(response) {
+        dateFinish = new Date();    //for end time of fetch operation
+        return response.json();
         //return response;
      })
      .then(function(response){
@@ -56,11 +64,14 @@ function getData (query) {
           console.log('body data present');
           console.log(response);
           console.log(response.currentConditions);
+
+          t_finish = dateFinish.getTime();  //record end time of fetch operation -success
           return(response);
         }
          else{
           noData = true;
           console.log('empty response body');
+          t_finish= getTime();  //record end time of fetch operation -fail
         } 
      })
      /**/ 
@@ -74,6 +85,8 @@ function getData (query) {
           setDisplayData(dataResult);
       */
           setDisplayData(response);
+          t_taken = t_finish - t_start;   //duration of fetch operation in milliseconds
+          console.log(`fetch data took ${t_taken} milliseconds`);
      })
          /* */
      .catch(e=> {
@@ -82,10 +95,24 @@ function getData (query) {
 }
 
 function setDisplayData (data) {
+
+  /* test 
+  const birthday = new Date(1725346200);
+  console.log(birthday);
+  const date1 = birthday.getDate();
+  console.log(date1);
+  // Expected output: 19
+  /* end of test */
+
+
+
   console.log(`data : ${data}`);
   //location
   siteDataObj.location= data.resolvedAddress;
-  //dateEpoch = 
+  //date
+  const dateNow = new Date();
+  console.log(dateNow);
+  //current conditions
   siteDataObj.CurrentConditions = data.currentConditions.conditions;
 
   console.log(siteDataObj);
